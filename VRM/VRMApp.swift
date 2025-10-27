bc0b675dd8ad197936a4f5916baee3f27d8cbf0b
longhd <longhd240195@gmail.com>
2025-10-27 21:08:01 +0700

    Initial Commit

---
+++ VRM/VRMApp.swift
@@ -0,0 +1,17 @@
+//
+//  VRMApp.swift
+//  VRM
+//
+//  Created by Nguyễn Hùng on 27/10/25.
+//
+
+import SwiftUI
+
+@main
+struct VRMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
